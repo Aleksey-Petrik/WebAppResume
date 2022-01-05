778c84dfdc9cfb1aef1b85520af57996aefb2c50
Aleksey Petrik <alexey.petric@gmail.com>
2022-01-05 17:16:30 +1000

    add checks for methods save, delete, get.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,29 +4,44 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private final Resume[] storage = new Resume[10_000];
+    private final int SIZE_STORAGE = 10_000;
+    private final Resume[] storage = new Resume[SIZE_STORAGE];
     private int size = 0;
 
     public void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size - 1, null);
         size = 0;
     }
 
     public void save(Resume r) {
-        storage[size] = r;
-        size++;
+        if (size != SIZE_STORAGE) {
+            if (findIndex(r.uuid) == -1) {
+                storage[size] = r;
+                size++;
+            } else {
+                System.out.printf("%nResume uuid=%s exists!%n", r.uuid);
+            }
+        } else {
+            System.out.println("The record limit has been exceeded!");
+        }
     }
 
     public Resume get(String uuid) {
         int indexResume = findIndex(uuid);
-        return indexResume != -1 ? storage[indexResume] : null;
+        if (indexResume != -1) {
+            return storage[indexResume];
+        }
+        System.out.printf("Resume uuid=%s not exists!%n", uuid);
+        return null;
     }
 
     public void delete(String uuid) {
         int indexResume = findIndex(uuid);
         if (indexResume != -1) {
-            System.arraycopy(storage, indexResume + 1, storage, indexResume, storage.length - size);
+            System.arraycopy(storage, indexResume + 1, storage, indexResume, SIZE_STORAGE - size);
             size--;
+        } else {
+            System.out.printf("Resume uuid=%s not exists!%n", uuid);
         }
     }
 
