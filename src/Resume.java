b7266198887d0ca10324967c86847bc92c7e0f33
Aleksey Petrik <alexey.petric@gmail.com>
2022-01-05 14:26:29 +1000

    first commit, begin

---
+++ src/Resume.java
@@ -0,0 +1,13 @@
+/**
+ * Initial resume class
+ */
+public class Resume {
+
+    // Unique identifier
+    String uuid;
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+}
