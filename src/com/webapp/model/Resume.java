fb6bfd2a6acfc3da2e7bd57b36b77fc781d39923
Aleksey Petrik <alexey.petric@gmail.com>
2022-03-01 19:38:07 +1000

    add jsp, list.jsp for out all resumes.

--- src/com/webapp/model/Resume.java
+++ src/com/webapp/model/Resume.java
@@ -63,6 +63,10 @@ public class Resume implements Comparable<Resume>, Serializable {
         return contacts;
     }
 
+    public String getContact(ContactType type) {
+        return contacts.get(type);
+    }
+
     public Map<SectionType, AbstractSection> getSections() {
         return sections;
     }
